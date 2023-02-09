ac092ec692d329a1445c30e42cc5fa889bc9c586
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-10 02:31:20 +0530

    Create convenience functions for the 6 heading levels

--- markdown/heading.go
+++ markdown/heading.go
@@ -36,3 +36,33 @@ func Heading(level int, contents ...string) *NodeHeading {
 func (heading *NodeHeading) String() string {
 	return strings.Repeat("#", heading.Level) + " " + heading.NodeText.String()
 }
+
+// H1 is a convenience function for creating a heading of level 1.
+func H1(contents ...string) *NodeHeading {
+	return Heading(1, contents...)
+}
+
+// H2 is a convenience function for creating a heading of level 2.
+func H2(contents ...string) *NodeHeading {
+	return Heading(2, contents...)
+}
+
+// H3 is a convenience function for creating a heading of level 3.
+func H3(contents ...string) *NodeHeading {
+	return Heading(3, contents...)
+}
+
+// H4 is a convenience function for creating a heading of level 4.
+func H4(contents ...string) *NodeHeading {
+	return Heading(4, contents...)
+}
+
+// H5 is a convenience function for creating a heading of level 5.
+func H5(contents ...string) *NodeHeading {
+	return Heading(5, contents...)
+}
+
+// H6 is a convenience function for creating a heading of level 6.
+func H6(contents ...string) *NodeHeading {
+	return Heading(6, contents...)
+}
