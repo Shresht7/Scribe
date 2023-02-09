3c6c55d0bff7f3c808d9963b6cb9a229f3150bee
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-10 01:21:05 +0530

    Reorganize code into a package
    Create the `scribe` package that contains the most basic tools for writing text documents

---
+++ scribe/document.go
@@ -0,0 +1,31 @@
+package scribe
+
+//* DOCUMENT *//
+
+// Document is just a collection of text nodes separated by a newline.
+type Document struct {
+	NodeText
+}
+
+// Instantiate a new document
+func NewDocument(opts ...*Document) *Document {
+	// Instantiate a new document with default options
+	document := &Document{
+		NodeText: NodeText{
+			Separator: "\n",
+		},
+	}
+
+	// If options are passed, override the defaults
+	if len(opts) > 0 {
+		options := opts[0]
+
+		// Override default options with user options
+		if options.Separator != "" {
+			document.Separator = options.Separator
+		}
+	}
+
+	// Return the document instance
+	return document
+}
