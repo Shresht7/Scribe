0d06b721c3eb013c531e29e2fba5651cf754e654
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-10 18:08:58 +0530

    Create `HorizontalRule` Node

---
+++ markdown/horizontalRule.go
@@ -0,0 +1,35 @@
+package markdown
+
+import "strings"
+
+//* HORIZONTAL RULE *//
+
+// NodeHorizontalRule represents a horizontal rule.
+type NodeHorizontalRule struct {
+	character rune
+	Repeat    int
+}
+
+// Instantiate a new horizontal rule with the given style
+func HorizontalRule(ch rune, rep int) *NodeHorizontalRule {
+	// If the character is not a valid character, set it to '-'
+	if ch != '*' && ch != '-' && ch != '_' {
+		ch = '-'
+	}
+
+	// If the repeat is less than 3, set it to 3
+	if rep < 3 {
+		rep = 3
+	}
+
+	// Create a new horizontal rule
+	return &NodeHorizontalRule{
+		character: ch,
+		Repeat:    rep,
+	}
+}
+
+// Implement the Node interface for NodeHorizontalRule
+func (hr *NodeHorizontalRule) String() string {
+	return strings.Repeat(string(hr.character), hr.Repeat)
+}
