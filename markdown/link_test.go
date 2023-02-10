e05cc870b0f2caa83f2e98a506a0720337faa418
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-11 00:02:40 +0530

    Create `Link` Node
    Fixes #7

---
+++ markdown/link_test.go
@@ -0,0 +1,42 @@
+package markdown
+
+import "testing"
+
+func TestLink(t *testing.T) {
+
+	// Create test cases
+	testCases := []struct {
+		description string
+		link        string
+		expected    string
+	}{
+		{
+			description: "Test link with title and url",
+			link:        Link("Title", "URL").String(),
+			expected:    "[Title](URL)",
+		},
+		{
+			description: "Test link with real title and url",
+			link:        Link("Google", "https://www.google.com").String(),
+			expected:    "[Google](https://www.google.com)",
+		},
+		{
+			description: "Test link with empty title and url",
+			link:        Link("", "").String(),
+			expected:    "[]()",
+		},
+		{
+			description: "Test link with relative url",
+			link:        Link("Relative", "/relative").String(),
+			expected:    "[Relative](/relative)",
+		},
+	}
+
+	// Run test cases
+	for _, testCase := range testCases {
+		if testCase.link != testCase.expected {
+			t.Errorf("Expected %s, got %s", testCase.expected, testCase.link)
+		}
+	}
+
+}
