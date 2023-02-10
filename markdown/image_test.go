56f1a7ad51d6f076e762a47525910a0ea5962d6f
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-11 00:02:52 +0530

    Create `Image` Node
    Fixes #9

---
+++ markdown/image_test.go
@@ -0,0 +1,42 @@
+package markdown
+
+import "testing"
+
+func TestImage(t *testing.T) {
+
+	// Create test cases
+	testCases := []struct {
+		description string
+		image       string
+		expected    string
+	}{
+		{
+			description: "Test image with alt and url",
+			image:       Image("Alt", "URL").String(),
+			expected:    "![Alt](URL)",
+		},
+		{
+			description: "Test image with real alt and url",
+			image:       Image("Google", "https://www.google.com").String(),
+			expected:    "![Google](https://www.google.com)",
+		},
+		{
+			description: "Test image with empty alt and url",
+			image:       Image("", "").String(),
+			expected:    "![]()",
+		},
+		{
+			description: "Test image with relative url",
+			image:       Image("Relative", "/relative").String(),
+			expected:    "![Relative](/relative)",
+		},
+	}
+
+	// Run test cases
+	for _, testCase := range testCases {
+		if testCase.image != testCase.expected {
+			t.Errorf("Expected %s, got %s", testCase.expected, testCase.image)
+		}
+	}
+
+}
