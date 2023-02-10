19885b4463223d250d560ef06f08bdaac933b7d5
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-10 17:49:47 +0530

    Create `Italic` Node

---
+++ markdown/italic.go
@@ -0,0 +1,28 @@
+package markdown
+
+import "github.com/Shresht7/Scribe/scribe"
+
+//* ITALIC *//
+
+// Italic represents italicized text.
+type NodeItalic struct {
+	scribe.NodeText
+}
+
+// Instantiate a new italic text with the given contents
+func Italic(contents ...scribe.Node) *NodeItalic {
+	// Create a new italic
+	italic := &NodeItalic{}
+	italic.WithSeparator(" ")
+
+	// Append the contents to the italic
+	italic.AppendChild(contents...)
+
+	// Return the italic
+	return italic
+}
+
+// Implement the Node interface for NodeItalic
+func (italic *NodeItalic) String() string {
+	return "*" + italic.NodeText.String() + "*"
+}
