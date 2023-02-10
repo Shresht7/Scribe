67d4953345b9d1a249533c22a8b03bc8c1af0108
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-10 23:24:08 +0530

    Create `StrikeThrough` Node

---
+++ markdown/strikethrough.go
@@ -0,0 +1,28 @@
+package markdown
+
+import "github.com/Shresht7/Scribe/scribe"
+
+//* STRIKETHROUGH *//
+
+// StrikeThrough is a strikethrough text.
+type NodeStrikeThrough struct {
+	scribe.NodeText
+}
+
+// Instantiate a new strikethrough text with the given contents
+func StrikeThrough(contents ...scribe.Node) *NodeStrikeThrough {
+	// Create a new strikethrough text
+	strikethrough := &NodeStrikeThrough{}
+	strikethrough.WithSeparator(" ")
+
+	// Append the contents to the strikethrough text
+	strikethrough.AppendChild(contents...)
+
+	// Return the strikethrough text
+	return strikethrough
+}
+
+// Implement the Node interface for NodeStrikeThrough
+func (bold *NodeStrikeThrough) String() string {
+	return "~~" + bold.NodeText.String() + "~~"
+}
