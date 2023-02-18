357c60cd1ddac8c1d19d527557eabdf27804aa15
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-18 18:15:59 +0530

    Add Examples

--- markdown/paragraph_test.go
+++ markdown/paragraph_test.go
@@ -1,9 +1,8 @@
 package markdown
 
 import (
+	"fmt"
 	"testing"
-
-	"github.com/Shresht7/Scribe/scribe"
 )
 
 func TestParagraph(t *testing.T) {
@@ -15,45 +14,45 @@ func TestParagraph(t *testing.T) {
 	}{
 		{
 			description: "Empty paragraph",
-			paragraph:   Paragraph(scribe.Text("")).String(),
+			paragraph:   Paragraph(Text("")).String(),
 			expected:    "",
 		},
 		{
 			description: "Paragraph with text",
-			paragraph:   Paragraph(scribe.Text("This is a test paragraph.")).String(),
+			paragraph:   Paragraph(Text("This is a test paragraph.")).String(),
 			expected:    "This is a test paragraph.",
 		},
 		{
 			description: "Paragraph with multiple text nodes",
 			paragraph: Paragraph(
-				scribe.Text("This is the first sentence."),
-				scribe.Text("This is the second sentence."),
+				Text("This is the first sentence."),
+				Text("This is the second sentence."),
 			).String(),
 			expected: "This is the first sentence.\nThis is the second sentence.",
 		},
 		{
 			description: "Paragraph with multiple text nodes and a separator",
 			paragraph: Paragraph(
-				scribe.Text("This is the first sentence."),
-				scribe.Text("This is the second sentence."),
+				Text("This is the first sentence."),
+				Text("This is the second sentence."),
 			).WithSeparator("\t").String(),
 			expected: "This is the first sentence.\tThis is the second sentence.",
 		},
 		{
 			description: "Paragraph with text and inline elements",
 			paragraph: Paragraph(
-				Bold(scribe.Text("This is some bold text")),
-				scribe.Text("This is some plain text"),
-				Italic(scribe.Text("This is some italic text")),
+				Bold(Text("This is some bold text")),
+				Text("This is some plain text"),
+				Italic(Text("This is some italic text")),
 			).String(),
 			expected: "**This is some bold text**\nThis is some plain text\n*This is some italic text*",
 		},
 		{
 			description: "Paragraph with text and block elements",
 			paragraph: Paragraph(
-				scribe.Text("This is some plain text"),
-				BlockQuote(scribe.Text("This is a blockquote")),
-				scribe.Text("This is some plain text"),
+				Text("This is some plain text"),
+				BlockQuote(Text("This is a blockquote")),
+				Text("This is some plain text"),
 			).String(),
 			expected: "This is some plain text\n> This is a blockquote\nThis is some plain text",
 		},
@@ -67,4 +66,33 @@ func TestParagraph(t *testing.T) {
 		}
 	}
 
+}
+
+func ExampleParagraph_simple() {
+	// Create a new paragraph
+	paragraph := Paragraph(
+		Text("This is a test paragraph."),
+	)
+
+	// Print the paragraph
+	fmt.Println(paragraph)
+
+	// Output: This is a test paragraph.
+}
+
+func ExampleParagraph_advanced() {
+	// Create a new paragraph
+	paragraph := Paragraph(
+		Text("This is the first sentence."),
+		Bold(Text("This is some bold text")),
+		Text("This is the second sentence."),
+	)
+
+	// Print the paragraph
+	fmt.Println(paragraph)
+
+	// Output:
+	// This is the first sentence.
+	// **This is some bold text**
+	// This is the second sentence.
 }
