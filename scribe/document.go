c4581382c2416c3e4229a60c6a81bd45c557d715
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-24 08:27:52 +0530

    Remove optional arguments from `NewDocument`

--- scribe/document.go
+++ scribe/document.go
@@ -8,21 +8,13 @@ type Document struct {
 }
 
 // Instantiate a new document
-func NewDocument(opts ...*Document) *Document {
+func NewDocument() *Document {
+
 	// Instantiate a new document with default options
 	document := &Document{}
 	document.WithSeparator("\n")
 
-	// If options are passed, override the defaults
-	if len(opts) > 0 {
-		options := opts[0]
-
-		// Override default options with user options
-		if options.Separator != "" {
-			document.Separator = options.Separator
-		}
-	}
-
 	// Return the document instance
 	return document
+
 }
